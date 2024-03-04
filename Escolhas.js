93a1f892ca518d1c68e8875d42f5f8cc037a7c71
Joao Fabrício Moya Alves <JoaoFabricioMoyaAlves@users.noreply.github.com>
2024-03-03 21:07:12 -0300

    adicionado array de atributos

--- Escolhas.js
+++ Escolhas.js
@@ -26,16 +26,51 @@ function Iniciar(){
     heroiMain.style.display = 'block';
      if(currentHeroAt.value == 1){
         heroiMain.style.backgroundImage = 'url("sprites/archer.png")';
+         //[vida/armadura/resistenciaMagica/danoFisico/DanoMagico/TaxaDeCritico]
+        let heroiAtributos = [100,10,10,20,0,5 ];
+
      }else if(currentHeroAt.value == 2){
         heroiMain.style.backgroundImage = 'url("sprites/warrior.png")';
+           
+        let heroiAtributos = [102,10,10,20,0,5 ];
+
      }else if(currentHeroAt.value ==3){
         heroiMain.style.backgroundImage = 'url("sprites/mage.png")';
+
+        let heroiAtributos = [103,10,10,20,0,5 ];
+
      }else{
         alert(`erro ao selecionar campeão`);
      }
 
+     let heroiAtributos = [0,0,0,0,0,0 ];
+
+     if(currentHeroAt.value == 1){
+        
+         //[vida/armadura/resistenciaMagica/danoFisico/DanoMagico/TaxaDeCritico]
+         heroiAtributos = [100,10,10,20,0,5 ];
+
+     }else if(currentHeroAt.value == 2){
+        
+           
+         heroiAtributos = [102,10,10,20,0,5 ];
+
+     }else if(currentHeroAt.value ==3){
+        
+
+         heroiAtributos = [103,10,10,20,0,5 ];
+
+     }else{
+        alert(`erro ao selecionar atributos`);
+     }
+
+
    escolha1.style.display = 'block';
    escolha2.style.display = 'block';
+
+   alert(`Atributos:${heroiAtributos}`)
+
+
 
 
 
